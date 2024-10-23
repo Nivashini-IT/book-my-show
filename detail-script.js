b7911427ad306a86fc6bef100ab6201db89f048d
Nivashini-IT <nivashsk04@gmail.com>
2024-10-23 20:52:30 +0530

    commit

--- detail-script.js
+++ detail-script.js
@@ -11,7 +11,7 @@ window.onload = async () => {
 async function fetchMovieDetails(movieId) {
   try {
     const response = await fetch(
-      `https://bookmyshow-backend-rofa.onrender.com/movie/${movieId}`
+      `https://movie-details-data.onrender.com/movie/${movieId}`
     );
     const movie = await response.json();
 
